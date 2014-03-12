eb055a028dd92c96f1fe48d0a38892bf8746ab1d
lideminismine <lideminismine@gmail.com>
2014-03-12 14:18:09 -0400

    transcode

--- twittube/views.py
+++ twittube/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django import forms
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+import boto
 
 import sys
 sys.path.append( '..' )
@@ -30,7 +31,23 @@ def handlefile(request):
             filename = str(s.id)+'_0.mp4'
             s.filename = filename
             s.save()
-            default_storage.save(filename, request.FILES['file'])
+            originalname = request.FILES['file'].name
+            default_storage.save(originalname, request.FILES['file'])
+            transcode = boto.elastictranscoder.connect_to_region("us-west-2",aws_access_key_id='AKIAJKADLVELVEBLGGGQ', aws_secret_access_key='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')
+	     params_in = { 'Key':    originalname,
+        	'FrameRate':        'auto',
+        	'Resolution':       'auto',
+        	'AspectRatio':      'auto',
+        	'Interlaced':       'auto',
+        	'Container':        'auto',
+    	     }
+
+            params_out =  { 'Key':  filename,
+        	'ThumbnailPattern': '',
+        	'Rotate':           'auto',
+        	'PresetId':         '1351620000001-000061',
+    	     }
+	     transcode.create_job(pipeline_id='assignment1', input_name=params_in, output=params_out)
             return HttpResponseRedirect(reverse('twittube.views.index'))
         else:
             return HttpResponse("upload form invalid")
