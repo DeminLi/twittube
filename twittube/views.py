5067b1a3ee3e2962b6f327b538294aaa0c8b5542
lideminismine <lideminismine@gmail.com>
2014-03-12 14:28:07 -0400

    transcode

--- twittube/views.py
+++ twittube/views.py
@@ -34,14 +34,19 @@ def handlefile(request):
             originalname = request.FILES['file'].name
             default_storage.save(originalname, request.FILES['file'])
             transcode = boto.elastictranscoder.connect_to_region("us-west-2",aws_access_key_id='AKIAJKADLVELVEBLGGGQ', aws_secret_access_key='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')
-	     params_in = { 'Key': originalname, 'FrameRate': 'auto', 'Resolution': 'auto', 'AspectRatio': 'auto', 'Interlaced': 'auto', 'Container': 'auto',}
+	    params_in = { 'Key': originalname,
+                          'FrameRate': 'auto',
+                          'Resolution': 'auto',
+                          'AspectRatio': 'auto',
+                          'Interlaced': 'auto',
+                          'Container': 'auto',}
 
             params_out =  { 'Key': filename,
         	'ThumbnailPattern': '',
         	'Rotate': 'auto',
         	'PresetId': '1351620000001-000061',
-    	     }
-	     transcode.create_job(pipeline_id='assignment1', input_name=params_in, output=params_out)
+    	    }
+	    transcode.create_job(pipeline_id='assignment1', input_name=params_in, output=params_out)
             return HttpResponseRedirect(reverse('twittube.views.index'))
         else:
             return HttpResponse("upload form invalid")
