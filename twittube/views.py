fe33cb3029c37360ef5da58e05a3fc9f88e1eb89
lideminismine <lideminismine@gmail.com>
2014-03-12 14:55:54 -0400

    transcode

--- twittube/views.py
+++ twittube/views.py
@@ -13,6 +13,20 @@ from conversation.models import Participant
 
 def index(request):
     all_sponsors = Sponsor.objects.all()
+    transcode = boto.elastictranscoder.connect_to_region("us-west-2",aws_access_key_id='AKIAJKADLVELVEBLGGGQ', aws_secret_access_key='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')
+    params_in = { 'Key': '_MOV0139.AVI',
+                  'FrameRate': 'auto',
+                  'Resolution': 'auto',
+                  'AspectRatio': 'auto',
+                  'Interlaced': 'auto',
+                  'Container': 'auto',}
+
+    params_out =  { 'Key': 'test.mp4',
+                    'ThumbnailPattern': '',
+                    'Rotate': 'auto',
+                    'PresetId': '1351620000001-000061',
+    }
+    transcode.create_job(pipeline_id='1394638986818-y3ixy5', input_name=params_in, output=params_out)
     return render(request, 'twittube/index.html', {'all_s':all_sponsors})
     #return HttpResponse("Hello, world. You're at the index.")
 
